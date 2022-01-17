5ad24fb9edd7a73d0bcf760086e7678a2385f7dd
santhoshshas <95738382+santhoshshas@users.noreply.github.com>
2022-01-17 20:45:01 +0530

    Add files via upload

---
+++ Assignment 4 (17-01-1022)/Breakinteger.java
@@ -0,0 +1,16 @@
+class Breakinteger
+{
+public static void main(String args[])
+{
+int a=5432;
+int d1=a/1000%10;
+int d2=a/100%10;
+int d3=a/10%10;
+int d4=a%10;
+System.out.println("Given input value :"+a);
+System.out.println("Sequence of given numbers :"+d1);
+System.out.println("Sequence of given numbers :"+d2);
+System.out.println("Sequence of given numbers :"+d3);
+System.out.println("Sequence of given numbers :"+d4);
+}
+}
---
+++ Assignment 4 (17-01-1022)/Compare.java
@@ -0,0 +1,10 @@
+class Compare
+{
+public static void main(String args[])
+{
+int a=55;
+int b=65;
+ boolean c=(a>b);
+System.out.println("a>b statement :"+c);
+}
+}
