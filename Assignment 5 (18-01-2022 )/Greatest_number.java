13e343f0e62ec3530284d4276cbe22220bf5acc1
santhoshshas <95738382+santhoshshas@users.noreply.github.com>
2022-01-18 22:57:46 +0530

    Add files via upload

---
+++ Assignment 5 (18-01-2022 )/Greatest_number.java
@@ -0,0 +1,22 @@
+class Greatest_number
+{
+public static void main(String args[])
+{
+int a=25,b=45,c=65;
+if(a>b)
+if(a>c)
+{
+System.out.println("The greatest number :"+a);
+}
+if(b>a)
+if(b>c)
+{
+System.out.println("The greatest number :"+b);
+}
+if(c>a)
+if(c>b)
+{
+System.out.println("The greatest number :"+c);
+}
+}
+}
