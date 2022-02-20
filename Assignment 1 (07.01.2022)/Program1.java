a14468d43a4a2c9e938da4b08eaf076ea117444f
santhoshshas <95738382+santhoshshas@users.noreply.github.com>
2022-02-21 00:40:45 +0530

    Delete Assignment 1 (07.01.2022) directory

--- Assignment 1 (07.01.2022)/Program1.java
+++
@@ -1,8 +0,0 @@
-class Program1
-{
-public static void main(String args[])
-{
-System.out.println("Hello");
-System.out.println("I'm Santhosh");
-}
-}
--- Assignment 1 (07.01.2022)/Program2.java
+++
@@ -1,9 +0,0 @@
-class Program2
-{
-public static void main(String args[])
-{
-int x=5,y=25;
-int z=x+y;
-System.out.println("Sum of two numbers :"+z);
-}
-}
